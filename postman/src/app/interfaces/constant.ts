ef383e39dc652d5603b8850caeab68dfdbf32ff5
SergeySalimov <salimov_s@tut.by>
2021-11-27 14:51:30 +0300

    PRJ-026 [ui] added container for view, and set time for messages

--- postman/src/app/interfaces/constant.ts
+++ postman/src/app/interfaces/constant.ts
@@ -18,8 +18,8 @@ export enum EYesOrNo {
   NO = 'No',
 }
 
-export const TIMER_FOR_ERROR_MESSAGE = 20;
-export const TIMER_FOR_INFO_MESSAGE = 15;
+export const TIMER_FOR_ERROR_MESSAGE = 5;
+export const TIMER_FOR_INFO_MESSAGE = 3;
 
 export const URL_REGEXP = 'https?:\\/\\/(www\\.)?[-a-zA-Z0-9@:%._\\+~#=]{1,256}\\.[a-zA-Z0-9()]{1,6}\\b([-a-zA-Z0-9()@:%_\\+.~#?&//=]*)';
 // export const URL_REGEXP = '(?:^|\\s)((https?:\\/\\/)?(?:localhost|[\\w-]+(?:\\.[\\w-]+)+)(:\\d+)?(\\/\\S*)?)';
