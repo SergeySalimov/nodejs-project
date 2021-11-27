465b7b33506bd9bdf7a4c4ef04488edf37ac9131
SergeySalimov <salimov_s@tut.by>
2021-11-27 13:21:29 +0300

    PRJ-025/2 [vitality] added correct bash for starter.sh

--- starter.sh
+++ starter.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-/home/user/nodejs-project/cross-env NODE_ENV=production node index.js &
+/home/user/nodejs-project/node_modules/cross-env/src/bin/cross-env.js NODE_ENV=production node /home/user/nodejs-project/index.js &
+
