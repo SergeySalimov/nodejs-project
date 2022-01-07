34067071ceabfdb3fdd986339858e965aa995a16
SergeySalimov <salimov_s@tut.by>
2022-01-07 22:22:46 +0300

    PRJ-046 [ui] added address for webSocket

--- postman/src/environments/environment.prod.ts
+++ postman/src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { RouteEnum } from '../app/app-routing.constant';
 
 export const environment = {
   production: true,
-  webSocketUrl: 'ws://178.172.195.18:7781',
+  webSocketUrl: 'ws://18.192.242.179:57781',
   URL_AFTER_LOGIN: `/${RouteEnum.STORAGE}`,
   MAX_FILE_SIZE: 1 * 1024 * 1024, // 1Mb
   FILES_LIMIT: 10,
